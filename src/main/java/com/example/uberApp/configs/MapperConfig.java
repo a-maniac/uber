1c79e0f73c1ba1e7b8c3706eb400bb983938700c
amanjoshi <ajoshi373@gmail.com>
2024-08-14 23:13:03 +0530

    converting Point to PointDto using modelMapper

--- src/main/java/com/example/uberApp/configs/MapperConfig.java
+++ src/main/java/com/example/uberApp/configs/MapperConfig.java
@@ -1,14 +1,35 @@
 package com.example.uberApp.configs;
 
+import com.example.uberApp.dto.PointDto;
+import com.example.uberApp.utils.GeometryUtil;
+import org.locationtech.jts.geom.Point;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import static com.example.uberApp.utils.GeometryUtil.createPoint;
 
 @Configuration
 public class MapperConfig {
 
     @Bean
     public ModelMapper modelMapper(){
-        return new ModelMapper();
+        ModelMapper mapper=new ModelMapper();
+
+        mapper.typeMap(PointDto.class, Point.class).setConverter(context->{
+            PointDto pointDto=context.getSource();
+            return GeometryUtil.createPoint(pointDto);
+        });
+
+        mapper.typeMap(Point.class, PointDto.class).setConverter(context->{
+            Point point=context.getSource();
+            double[] coordinates={
+                    point.getX(),
+                    point.getY()
+            };
+            return new PointDto(coordinates);
+        });
+
+        return mapper;
     }
 }
--- src/main/java/com/example/uberApp/utils/GeometryUtil.java
+++ src/main/java/com/example/uberApp/utils/GeometryUtil.java
@@ -1,2 +1,18 @@
-package com.example.uberApp.utils;public class GeometryUtil {
+package com.example.uberApp.utils;
+
+import com.example.uberApp.dto.PointDto;
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.Point;
+import org.locationtech.jts.geom.PrecisionModel;
+
+public class GeometryUtil {
+
+    public static Point createPoint(PointDto pointDto){
+        GeometryFactory geometryFactory=new GeometryFactory(new PrecisionModel(),4326);
+        Coordinate coordinate= new Coordinate(pointDto.getCoordinates()[0],
+                pointDto.getCoordinates()[1]);
+
+        return geometryFactory.createPoint(coordinate);
+    }
 }
